19dde451aa0b5e454b4022c51d7e4b1980aa6408
diegous <munoa.d@gmail.com>
2019-03-30 16:26:18 -0300

    Added style to Game Index page. Added button animation

--- public/views/pages/GamesIndex.js
+++ public/views/pages/GamesIndex.js
@@ -1,31 +1,58 @@
 import Utils from '../../services/Utils.js';
 import API   from '../../services/API.js';
 
-const renderForm = () {
+const renderForm = () => {
   return `
-      <form id="new-game-form">
-        <label for="email">Email</label>
-        <input type="text" name="email" id="email"></input>
-        <br/>
+    <div class="new-game-buttons">
+      <div class="btn-container">
+        <button type="button" value="Easy" class="btn">Easy</button>
+      </div>
+      <div class="btn-container">
+        <button type="button" value="Medium" class="btn">Medium</button>
+      </div>
+      <div class="btn-container">
+        <button type="button" value="Hard" class="btn">Hard </button>
+      </div>
+    </div>
 
-        <label for="password">Password</label>
-        <input type="text" name="password" id="password"></input>
-        <br/>
+    <br/>
 
-        <button type="button" value="Login" class="btn"></input>
-      </form>
-    `
+    <div class="new-game-form-container">
+      <fieldset>
+        <legend>Custom</legend>
+        <form id="new-game-form">
+          <label for="width">Width</label>
+          <input type="number" min="2" max="999" name="width" id="width" value="9"></input>
+          <br/>
+
+          <label for="height">Height</label>
+          <input type="number" min="2" max="999" name="height" id="height" value="9"></input>
+          <br/>
+
+          <label for="bombs">Bombs</label>
+          <input type="number" name="bombs" id="bombs" value="10"></input>
+          <br/>
+
+          <div class="btn-container">
+            <button type="button" class="btn">Start</button>
+          </div>
+        </form>
+      </fieldset>
+    </div>
+  `
 }
 
 const renderGame = (game) => {
   return `
     <li>
-      <button type="button" data-id="${ game.id }" class="btn">
-        <span class="game-${ game.state }">
-          ${ game.state }
-        </span>
-        - ${ game.height } by ${ game.height }
-      </button>
+      <div class="btn-container">
+        <button type="button" data-id="${ game.id }" class="btn">
+          <span class="game-${ game.state }">
+            ${ game.state }
+          </span>
+          - ${ game.height } by ${ game.height }
+        </button>
+      </div>
     </li>
   `;
 }
@@ -36,11 +63,8 @@ const GamesIndex = {
     const games = await response.json();
 
     return `
-      <h1>Games</h1>
-
       <h2>New Game</h2>
-      <form>
-      </form>
+      ${ renderForm() }
 
       <h2>Finished Games</h2>
       <ul class="game-list">
